dad5214c3baf6e403d8b1237da506014c9f14bfe
Mateusz Ozog <mateusz2345678@gmail.com>
2020-03-30 23:05:49 +0200

    :tada: init commit Svelte is great!

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	import User from "./User.svelte";
+
+	let name = "Dave";
+	let nameChanged = false;
+	let fullName;
+
+	let user = {
+		name: "Mateusz",
+		email: "mateusz2345678@gmail.com"
+	};
+
+	//Update every update
+	$: {
+		fullName = name + " Dee";
+		console.log("Name changed!");
+	}
+
+	const changeName = () => {
+		name = "Tim";
+		nameChanged = true;
+	};
+</script>
+
+<main>
+	{#if nameChanged}
+		<p>Name was changed!</p>
+	{:else}
+		<p>Name is not changed!</p>
+	{/if}
+
+	<h2>Hello {name}!</h2>
+	<p>(full name: {fullName})</p>
+	<button on:click={changeName}>Change name</button>
+	<hr />
+	<h2>User</h2>
+	<User {user} />
+</main>
---
+++ src/User.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let user;
+</script>
+
+<div>
+	<p>User name: {user.name}</p>
+	<p>User email: {user.email}</p>
+</div>
